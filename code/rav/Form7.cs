2f8655cae7c4f1bbd01ebb90d21c64370231e8ba
Student-Raso <a20311079@uthermosillo.edu.mx>
2021-11-03 16:21:55 -0700

    other

--- code/rav/Form7.cs
+++ code/rav/Form7.cs
@@ -14,5 +14,25 @@ namespace rav
         {
             InitializeComponent();
         }
+        WebBrowser webBrowser1 = new WebBrowser();
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Left = 12;
+            webBrowser1.Top = 270;
+            webBrowser1.Width = 770;
+            webBrowser1.Height = 160;
+            webBrowser1.ScrollBarsEnabled = true;
+            webBrowser1.Visible = true;
+            webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                | System.Windows.Forms.AnchorStyles.Left)
+                | System.Windows.Forms.AnchorStyles.Right)));
+            this.Controls.Add(this.webBrowser1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad", "");
+        }
     }
 }
